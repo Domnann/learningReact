8ea69f0af198b10bdaf69bc661452cae3a38887e
Domnann <domnangambo@gmail.com>
2022-08-06 22:00:58 +0100

    worked on style

--- dom-blog/src/Home.js
+++ dom-blog/src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+
+    const handleClick =()=> {
+        console.log("Hello World");
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick= {handleClick}>Click Me!</button>
         </div>
      );
 }
