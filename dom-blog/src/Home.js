c1d00485f845c9ead547b0f328b4286a1b35d5ac
Domnann <domnangambo@gmail.com>
2022-08-10 15:54:05 +0100

    worked on post in home.js

--- dom-blog/src/Home.js
+++ dom-blog/src/Home.js
@@ -1,12 +1,25 @@
-const Home = () => {
+import { useState } from "react";
+import BlogList from "./Bloglist";
 
-    const handleClick =()=> {
-        console.log("Hello World");
-    }
+
+const Home = () => {
+    // let name = "Mario"
+    // const [name, setName] = useState('Mario');
+    // const [age, setAge] = useState(25);
+
+    // const handleClick =()=> {
+    //     setName("Luigi");
+    //     setAge(30);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]);
+    
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick= {handleClick}>Click Me!</button>
+            <BlogList blogs = {blogs} title = "All Blogs!" />
         </div>
      );
 }
