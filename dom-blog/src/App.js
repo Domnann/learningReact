4f731a237baee302c9c2621bcc1b278c2a528094
Domnann <domnangambo@gmail.com>
2022-08-15 20:37:27 +0100

    worked on blog detail page

--- dom-blog/src/App.js
+++ dom-blog/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from "./create";
+import BLogDetails from "./BlogDetails";
 
 function App() {
   const title = "Welcome to Domnan's Blog"
@@ -12,12 +13,16 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
+            <Route path="/blogs/:id">
+              <BLogDetails />
+            </Route>
             <Route path="/create">
               <Create />
             </Route>
             <Route path="/">
               <Home />
             </Route>
+            
           </Switch>
         </div>
       </div>
--- dom-blog/src/Bloglist.js
+++ dom-blog/src/Bloglist.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BlogList = ({blogs, title, handleDelete}) => {
     // const blogs = props.blogs;
@@ -10,9 +11,10 @@ const BlogList = ({blogs, title, handleDelete}) => {
         <h2>{ title }</h2>
         {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>written by { blog.author }</p>
-                    
+                    <Link to = {`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}
        </div>
